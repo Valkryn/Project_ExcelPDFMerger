ed2a8f373eef44d89a4e2c773c3c512b60915285
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2024-05-12 19:22:38 -0400

    refactored workdata and main app

--- excel_main.py
+++ excel_main.py
@@ -3,7 +3,7 @@ from math import isnan
 from pdf_main import create_pdf
 from tkinter import Tk, messagebox  # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
-from work_data import *
+from work_data import address_map
 
 Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
 
@@ -15,12 +15,6 @@ def select_file(prompt_message, file_name, file_type):
     return file
 
 
-def add_leading_zeroes(value):
-    while len(value) != 4:
-        value = '0' + value
-    return value
-
-
 # show an "Open" dialog box and return the path to the selected file
 messagebox.showinfo(title='Excel To PDF', message='Please select an excel file to merge')
 user_selected_file = select_file('Select Excel file', 'Excel File', '*.xlsx')
@@ -29,8 +23,6 @@ excel_file = pd.read_excel(user_selected_file)
 
 messagebox.showinfo(title='Excel To PDF', message='Please select the PDF form.')
 selected_pdf = select_file('Select PDF form', 'PDF form', '*.pdf')
-if selected_pdf == '':
-    quit()
 
 columns_to_select = ['First Name:', 'Last Name:', 'Last 4:', 'Ext/Tel:', 'E-mail Address:', 'Years:', 'Building:',
                      'Employee Number', 'Address', 'City', 'State', 'ZipCode']
@@ -41,23 +33,16 @@ list_of_objects = selected_columns.to_dict(orient='records')
 count = 0
 
 while count < len(list_of_objects):
-    if list_of_objects[count]['Building:'] == 'Kennedy':
-        list_of_objects[count]['Work_Address'] = work_ken_address
-    elif list_of_objects[count]['Building:'] == 'Price':
-        list_of_objects[count]['Work_Address'] = work_price_address
-    elif list_of_objects[count]['Building:'] == 'Van Etten':
-        list_of_objects[count]['Work_Address'] = work_van_address
-    else:
-        list_of_objects[count]['Work_Address'] = work_forch_address
-
-    list_of_objects[count].update(work_dict)
+    building_name = list_of_objects[count]['Building:']  # will return name of building
+    list_of_objects[count]['Work_Address'] = address_map.get(building_name)  # looks up address via get
+    list_of_objects[count].update(address_map['work_dict'])
 
     if isnan(list_of_objects[count]['Last 4:']):  # uses the math module
         list_of_objects[count]['Last 4:'] = ''
     else:
         list_of_objects[count]['Last 4:'] = int(list_of_objects[count]['Last 4:'])
         while len(str(list_of_objects[count]['Last 4:'])) != 4:
-            formatted_ssn = add_leading_zeroes(str(list_of_objects[count]['Last 4:']))
+            formatted_ssn = str(list_of_objects[count]['Last 4:']).zfill(4)
             list_of_objects[count]['Last 4:'] = formatted_ssn
 
     create_pdf(list_of_objects[count], selected_pdf)
--- work_data.py
+++ work_data.py
@@ -1,7 +1,9 @@
-work_forch_address = '1300 Morris Park Avenue'
-work_ken_address = '1410 Pelham Parkway South'
-work_price_address = '1301 Morris Park Avenue'
-work_van_address = '1225 Morris Park Avenue'
-work_dict = {'Work_City': 'Bronx',
-             'Work_State': 'NY',
-             'Work_Zipcode': '10461'}
+address_map = {
+    "Forch": '1300 Morris Park Avenue',
+    "Kennedy": '1410 Pelham Parkway South',
+    "Price": '1301 Morris Park Avenue',
+    "Van Etten": '1225 Morris Park Avenue',
+    "work_dict": {'Work_City': 'Bronx',
+                'Work_State': 'NY',
+                'Work_Zipcode': '10461'}
+}
